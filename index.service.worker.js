fd01f964f0bb672fd40ce3bba9af54abbac2fccb
surfacedfox <surfacedfox@gmail.com>
2026-04-18 16:06:03 +0000

    Deploying to gh-pages from @ surfacedfox/LD59@fe1f2bd33c0941fe4f0efd400fa8c2ab2f1e2dc9 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776523549|3494711';
+const CACHE_VERSION = '1776528353|3480299';
 /** @type {string} */
 const CACHE_PREFIX = 'LD59-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
