518bfeb6b1641a27e18d6474aa8c4ec99a30f724
Richard Snider <lighthousemaniac@gmail.com>
2026-04-18 20:26:23 -0600

    fix external load issue

--- scripting/images.gd
+++ scripting/images.gd
@@ -1,15 +1,14 @@
 class_name ImageSingleton extends Node
 
 
-var textures: Dictionary[String, ImageTexture] = {}
+var textures: Dictionary[String, Texture2D] = {}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for c in range(8):
 		var full_name = "shape_" + String.chr(97 + c)
-		var img = Image.load_from_file("res://textures/" + full_name + ".png")
-		textures[full_name] = ImageTexture.create_from_image(img)
+		textures[full_name] = load("res://textures/" + full_name + ".png")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
