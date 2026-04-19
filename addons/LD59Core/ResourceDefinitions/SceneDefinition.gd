abad02db15ee0534f10fa0834b6bfb7243ee0fab
surfacedfox <asheos@axomem.io>
2026-04-19 10:34:47 +0530

    Resource setup

---
+++ addons/LD59Core/ResourceDefinitions/SceneDefinition.gd
@@ -0,0 +1,10 @@
+class_name SceneDefinition
+extends Resource
+
+@export var sceneOrder: Array[PackedScene]
+
+# Make sure that every parameter has a default value.
+# Otherwise, there will be problems with creating and editing
+# your resource via the inspector.
+func _init(p_sceneOrder = []):
+	sceneOrder = p_sceneOrder
